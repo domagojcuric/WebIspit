93941e1f2961752092d3f7354e561abd808a372a
Domagoj <Domagoj@192.168.1.5>
2017-09-19 20:24:44 +0200

    funkcija signin

---
+++ class/users.php
@@ -0,0 +1,47 @@
+<?php
+
+session_start();
+class users{
+    public $host="localhost";
+    public $username="root";
+    public $pass="";
+    public $db_name="quiz_oops";
+    public $conn;
+    
+    public function __construct()
+    {
+        $this->conn=new mysqli($this->host, $this->username, $this->pass, $this->db_name);
+        if($this->conn->connect_errno)
+        {
+            die  ("databases connection failed".$this->conn->connect_errno);
+        }
+    }
+          
+    public function signup($data)
+    {
+       $this->conn->query($data) ;
+       return true;
+    }
+    public function signin($email,$pass)
+    {
+        $query=$this->conn->query("SELECT email,pass FROM signup WHERE email='$email' AND pass='$pass'");
+        $query->fetch_array(MYSQLI_ASSOC);
+        if ($query->num_rows>0)
+        {
+            $_SESSION['email']=$email;
+            return TRUE;
+        }
+        else 
+        {
+            return FALSE;
+        }
+            
+        }
+    public function url($url)
+       {
+        header("location:".$url);
+       }
+}
+
+
+?>
