32d5c828f8bb9928ccaffc038a0bfd8dfa6837d1
Domagoj <Domagoj@192.168.1.5>
2017-09-19 20:25:16 +0200

    sitnice

--- signup_sub.php
+++ signup_sub.php
@@ -1,14 +1,14 @@
 <?php
-include "class/users.php";
+include 'class/users.php';
 $register=new users;
 extract($_POST);
 $img_name=$_FILES["img"]["name"];
 $tmp_name=$_FILES["img"]["tmp_name"];
 move_uploaded_file($tmp_name, "img/".$img_name);
-$query="INSERT INTO singup VALUES ('','$n','$e','$p','$img_name')";
-if($register->sigup($query))
+$query="INSERT INTO signup VALUES ('','$n','$s','$e','$p','$img_name')";
+if($register->signup($query))
     {
-    $register->url("index.php?run=seccess");
+    $register->url("index.php?run=success");
     }
 
 ?>
