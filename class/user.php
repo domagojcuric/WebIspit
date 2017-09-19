3bb00d167f834c61a13b0d08e0e2b9815d2cde46
Domagoj <Domagoj@192.168.1.5>
2017-09-19 15:18:12 +0200

    user pravim

---
+++ class/user.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class users{
+    public $host="localhost";
+    public $username="root";
+    public $pass="";
+    public $db_name="quiz_oops";
+    
+    public function __construct()
+    {
+        $this->conn=new mysqli($this->host, $this->username, $this->pass, $this->db_name);
+        if($this->conn->connect_errno)
+        {
+            die  ("databases connection failed".$this->conn->connect_errno);
+        }
+    }
+          
+    public function singup($data)
+    {
+       $this->conn->query($data) ;
+       return true;
+    }
+}
+
+new user;
+
+?>
