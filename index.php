bf5b8a202bb7965089bd496fa9564a0105baa3e1
Domagoj <Domagoj@192.168.1.5>
2017-10-17 14:08:02 +0200

    napravljen al css se može napravit za ove ssa strane

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Online.ispit</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -17,8 +17,8 @@
     <div class="row">
         <div class="col-sm-12">
             <div class="panel panel-danger">
-                  <div class="panel-heading">Online ispit napravljen u php-u</div>
-                  <div class="panel-body">Ispit</div>
+                <div class="panel-heading"><h3 align="center">Dobrodošli na stranicu Online.ispit</h3></div>
+                  
             </div>
         </div>
     </div>    
@@ -26,7 +26,12 @@
     
 <div class="container">
     <div class="row">
-	<div class="col-sm-6"><br>
+        <div class="col-sm-4"><br><br><br>
+            <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/95188-200.png"/>
+                    
+            <h6 ><a href="adminlog.php">Prijavi se kao ADMIN</a></h6>
+	</div> 
+	<div class="col-sm-4"><br>
 <br>
 <br>
             <div class="panel panel-info">
@@ -48,51 +53,17 @@
                         <input type="password" class="form-control" name="p" id="pwd" placeholder="Unesi lozinku">
                     </div>
                         <button type="submit" class="btn btn-default">Potvrdi</button>
+                        <p><br>Novi korisnik? <a href="index_reg.php"> Registriraj se</a> Besplatno</p>
 		</form>
                 </div>
             </div>
 	</div> 
-        <div class="col-sm-6">
-<br>
-<br>
-            <div class="panel panel-info">
-                
-                <div class="panel-heading">Izradi račun</div>
-                <div class="panel-body">
-                    <?php 
+        	<div class="col-sm-4">
+                   <br> <br> <br>  <br> 
+                    <img src="http://iz.unizd.hr/Portals/70/images/ikone/studomat.png"/>
                     
-                    if(isset($_GET['run'])&& $_GET['run']=="success")
-                        {
-                            echo "<mark>Your registration successfully  done</mark>";
-                        }
-                    
-                    ?>
-                    <form method="post" action="signup_sub.php" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="name">Ime:</label>
-                        <input type="name" class="form-control" name="n" id="name" placeholder="Unesi ime">
-                    </div>
-                    <div class="form-group">
-                        <label for="surname">Prezime:</label>
-                        <input type="surname" class="form-control" name="s" id="surname" placeholder="Unesi prezime">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="email" class="form-control" name="e" id="email" placeholder="Unesi mail">
-                    </div>
-                    <div class="form-group">
-                        <label for="pwd">Lozinka:</label>
-                        <input type="password" class="form-control" name="p" id="pwd" placeholder="Unesi lozinku">
-                    </div>
-                    <div class="form-group">
-                        <label for="image">Dodaj sliku:</label>
-                        <input type="file" class="form-control" name="img" id="file">
-                    </div>
-                        <button type="submit" class="btn btn-default">Potvrdi</button>
-		</form>
-                </div>
-            </div>
-	</div>
+                    <h2><br><a href="https://www.isvu.hr/studomat/prijava">&nbsp;&nbsp;&nbsp;&nbsp;PRIJAVI ISPIT&nbsp;&nbsp;&nbsp;&nbsp;</a></h2>
+	</div> 
     </div>
 </div>
 
