dc11e1728827ffdb3a09d204ab76a3c68a12a241
Domagoj <Domagoj@192.168.1.5>
2017-10-20 13:19:17 +0200

    da ne skace

--- admin/php_action/update_cat.php
+++ admin/php_action/update_cat.php
@@ -12,9 +12,7 @@ if($_POST) {
  
     $sql = "UPDATE category SET cat_name = '$cat_name' WHERE id = {$id}";
     if($connect->query($sql) === TRUE) {
-        echo "<p>Succcessfully Updated</p>";
-        echo "<a href='../edit_cat.php?id=".$id."'><button type='button'>Back</button></a>";
-        echo "<a href='../show_que'><button type='button'>Home</button></a>";
+        header('Location: http://localhost:8000/admin/kategorije.php');
     } else {
         echo "Erorr while updating record : ". $connect->error;
     }
